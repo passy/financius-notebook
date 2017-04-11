629c0ccd6dc0e457ef3615bc606b6eb5a0901e00
Pascal Hartig <phartig@rdrei.net>
2017-04-11 17:31:48 +0100

    Update jupyter

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-docker run --rm -it -p 8888:8888 -v "$(pwd):/home/jovyan/work" $@ jupyter/scipy-notebook:e736784a1a8f
+docker run --rm -it -p 8888:8888 -v "$(pwd):/home/jovyan/work" $@ jupyter/scipy-notebook:eb70bcf1a292
